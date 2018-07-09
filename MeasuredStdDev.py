ae5181ac03398ff9368f21d4a6b4809ff70e08d7
hayaalsh <haya.a.alsh@gmail.com>
2018-07-09 23:51:52 +0300

    senario 6

--- MeasuredStdDev.py
+++ MeasuredStdDev.py
@@ -1,18 +1,19 @@
 import fileinput
 import pandas
-import numpy as np
 
 GPSPosXY = pandas.read_csv("./config/log/Graph1.txt", sep=",", header=None, skiprows=1)
 AccelXY  = pandas.read_csv("./config/log/Graph2.txt", sep=",", header=None, skiprows=1)
 
+_ , MeasuredStdDev_GPSPosXY = GPSPosXY.std(axis=0)
+_ , MeasuredStdDev_AccelXY = AccelXY.std(axis=0)
 
-print(str(GPSPosXY.std(axis=0)))
-
-print(str(AccelXY.std(axis=0)))
+print('MeasuredStdDev_GPSPosXY = %0.2f' % MeasuredStdDev_GPSPosXY)
+print('MeasuredStdDev_AccelXY = %0.2f' % MeasuredStdDev_AccelXY)
 
 #for line in fileinput.input("./config/06_SensorNoise.txt", inplace=True):
 #	if line.strip().startswith('MeasuredStdDev_GPSPosXY = '):
-#		line = 'MeasuredStdDev_GPSPosXY = %0.2f\n' % GPSPosXY.std(axis=0)
+#		line = 'MeasuredStdDev_GPSPosXY = %0.2f' % MeasuredStdDev_GPSPosXY
 #	elif line.strip().startswith('MeasuredStdDev_AccelXY = '):
-#		line = 'MeasuredStdDev_AccelXY = %0.2f\n' % AccelXY.std(axis=0)
+#		line = 'MeasuredStdDev_AccelXY = %0.2f' % MeasuredStdDev_AccelXY
 #	print line,
+
